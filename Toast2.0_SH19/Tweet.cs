b4b2190ba51dbe3e7084e577a4a467afc033352f
ahsanrohan <ahsanrohan@ufl.edu>
2019-09-21 02:25:30 -0400

    Created class Tweet

---
+++ Toast2.0_SH19/Tweet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toast2._0_SH19
+{
+    class Tweet
+    {
+        string tweet;
+
+        public Tweet()
+        {
+
+        }
+
+        public Tweet(string _tweet)
+        {
+            tweet = _tweet;
+        }
+
+        private string getTweet()
+        {
+            return tweet;
+        }
+
+        private void analyze(List<Tweet> allTweets)
+        {
+
+        }
+    }
+}
