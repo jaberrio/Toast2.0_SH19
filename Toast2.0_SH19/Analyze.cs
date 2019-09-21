728a133d1de69ae36d8f25b87bacce211d548cfb
Julian Berrio <ja.berrio@ufl.edu>
2019-09-21 17:16:21 -0400

    Corrected sort fuction for Anc and removed method because it is one line now.

--- Toast2.0_SH19/Analyze.cs
+++ Toast2.0_SH19/Analyze.cs
@@ -170,29 +170,23 @@ namespace Toast2._0_SH19
                 tempList.sadness += f.sadness;
                 tempList.size++;
             }
-            //sort();
+
+            //Sort the world
+            wordCountList = wordCountList.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
             return tempList;
         }
+
         private Dictionary<string, int> wordCountList = new Dictionary<string, int>();
-       // private Array[,] topTen = new Array[10, 2];
+        
         public void countWord(string word)
         {
             if (wordCountList.ContainsKey(word))
                 wordCountList[word]++;
             else
                 wordCountList[word] = 1;
-            //private
-            //return wordCountList;
+            
         }
-        public void  sort()
-        {/*
-            wordCountList.OrderByDescending<string, int>
-            foreach (KeyValuePair<string, int> pair in wordCountList)
-            {
-                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
-            }*/
-        }
-        //public List
     
     
         public Dictionary<string, double> getPersonaities(TTweetList _list)
