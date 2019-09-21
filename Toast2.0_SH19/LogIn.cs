8fb2f5532b6e36ea0c50d5c4ebfb1f3219e36f29
Julian Berrio <ja.berrio@ufl.edu>
2019-09-21 02:16:40 -0400

    Commit Inital

--- Toast2.0_SH19/LogIn.cs
+++ Toast2.0_SH19/LogIn.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Toast2._0_SH19
 {
-    public partial class Form1 : Form
+    public partial class LogIn : Form
     {
-        public Form1()
+        public LogIn()
         {
             InitializeComponent();
         }
--- Toast2.0_SH19/Program.cs
+++ Toast2.0_SH19/Program.cs
@@ -16,7 +16,7 @@ namespace Toast2._0_SH19
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LogIn());
         }
     }
 }
