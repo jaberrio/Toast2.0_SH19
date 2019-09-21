1eb5627e38de523b8650375773a78311e1f04818
ahsanrohan <ahsanrohan@ufl.edu>
2019-09-21 04:29:58 -0400

    Added Analyze class

---
+++ Toast2.0_SH19/Analyze.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toast2._0_SH19
+{
+    class Analyze
+    {
+        IEnumerable<string> tweet;
+        IEnumerable<string> user;
+        public Analyze()
+        {
+
+        }
+        public Analyze(IEnumerable<string> _tweet, IEnumerable<string> _user)
+        {
+            tweet = _tweet;
+            user = _user;
+        }
+    }
+}
