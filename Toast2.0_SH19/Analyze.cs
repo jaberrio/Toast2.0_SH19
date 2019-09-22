3f5c7ed6b5a99ae5907b900ccad10c380efa6787
ahsanrohan <ahsanrohan@ufl.edu>
2019-09-21 23:46:07 -0400

    Fixed formatting of positivity

--- Toast2.0_SH19/Analyze.cs
+++ Toast2.0_SH19/Analyze.cs
@@ -352,7 +352,7 @@ namespace Toast2._0_SH19
 
         public double getPositivity(TTweetList _list)
         {
-            return _list.positivity;
+            return (Math.Round((double)_list.positivity, 2) * 100);
         }
     }
 }
